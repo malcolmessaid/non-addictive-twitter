c7de6bd114ae93586e80513308209066ab106fc1
malcolm <malcolmessaid1@gmail.com>
2022-02-26 11:06:37 -0600

    clean up

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const frontEndDir = require('path').join(__dirname, "frontend/");
 // TODO: FIGURE OUT HOW TO AUTHROZIE FETCH REQUEST TO TWITTER
 
 const ACCOUNTS = {'nathanaclark': 2407905670}
-const TIMELINE_URL = `https://api.twitter.com/2/users/:id/tweets?max_results=5&expansions=attachments.media_keys,referenced_tweets.id`
+const TIMELINE_URL = `https://api.twitter.com/2/users/:id/tweets?max_results=10&expansions=attachments.media_keys,referenced_tweets.id`
 
 
 app.listen(port, () => {
@@ -25,23 +25,32 @@ app.listen(port, () => {
 const tweet_id = 1496087033093230599
 app.get('/', async (req, res) =>
 {
-  console.log(env.malcolm);
+
+  // Pulling From Tweitter
   res.sendFile('index.html', { root: frontEndDir})
-  var response = await pull_timeline_outer(2407905670)
-  // var tweet_res = await pull_tweet(1496087033093230599)
-  // console.log(res.json(response.body));
-  console.log(JSON.parse(response));
-  // console.log(tweet_res.text());
-  // console.log(Object.keys(response)  );
+  var response = await pull_timeline_outer(2407905670) // Pull recent tweets
+  // TODO: PARSE RESPOSNE
+      // check if references tweet
+      // check if referenced tweet are in database
+  // TODO: CEHCK WHICH WARE NEW (Have api rquest for latestest )
+  // TODO:  SEND NEW
+  // console.log(response);
+  console.log(response.includes);
+
+  // Loading into my Database/chekcing agsint my database
   // Load from my databsase the most recent
+  // from express laoding data for user
 })
 
-return
+
+
 // CronJob to run every X-amount of time
-var job = new CronJob('*/5 * * * * *', function()
-{
-  console.log('You will see this message every 5 seconds');
-}, null, true, 'America/Los_Angeles');
+// var job = new CronJob('*/5 * * * * *', function()
+// {
+//   console.log('You will see this message every 5 seconds');
+// }, null, true, 'America/Los_Angeles');
+
+
 
 
 // Pulls recent tweets from given user
@@ -56,43 +65,25 @@ async function pull_tweet(id)
   }
   var res = await fetch(str, options).then(res => res.text())
     .then(data => {
-      // const result = convert.xml2js(data);
-      console.log("result in json: ", data);
-      return data
+      return JSON.parse(data)
     })
-
-    // .then((res) =>
-    // {
-    //   console.log(res.text());
-    //   return res.text()
-    // }).catch(err => {
-    //   console.log("err", err)
-    //   return err
-    // });
-  // return res
-
 }
 
 
 // Pulls recent tweets from given user
 async function pull_timeline_outer(id)
 {
-  const str = `https://api.twitter.com/2/users/2407905670/tweets?max_results=5&expansions=attachments.media_keys,referenced_tweets.id`
-  console.log(str);
+  const str = `https://api.twitter.com/2/users/${id}/tweets?max_results=10&expansions=attachments.media_keys,referenced_tweets.id`
   res = await fetch(str,
   {
-    // mode: 'no-cors',
     headers: new fetch.Headers({
         'User-Agent':'art_project_2_3',
         'Authorization': env.bearer,
     }),
   }).then(res => res.text())
     .then(data => {
-      // const result = convert.xml2js(data);
-      // console.log("result in json: ", data);
-      return data
+      return JSON.parse(data)
     })
-  // console.log(res);
   return res
 
 }
