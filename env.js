9da01b77a4c8f92881dc9b251e5b381410e407f5
M. Malcolm Essaid <32280995+malcolmessaid@users.noreply.github.com>
2022-02-28 09:50:42 -0600

    Delete env.js

--- env.js
+++
@@ -1,5 +0,0 @@
-var env = {
-  malcolm : "ma",
-  bearer : "Bearer AAAAAAAAAAAAAAAAAAAAAHT0ZQEAAAAAGW%2F%2FyVPziqJ6QK%2B6%2Fvwt0HZJ5W0%3DvGAsz6KjxywxGHJ5DKP4aqun4N2VZA8Ubzzj182RRpoItUZXPn",
-}
-module.exports = {env};
